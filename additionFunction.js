bdb300e064bae6b8e73d3045a85ec010c57d19c5
Hamxa-Khalid <hamxi8381@gmail.com>
2023-07-26 22:34:41 +0500

    Created program of Addition Function

---
+++ additionFunction.js
@@ -0,0 +1,18 @@
+//Write a function that takes two numbers as arguments and returns their sum.
+
+//Name less Function
+const nameLess = function (a, b) {
+  return a + b;
+};
+
+function Sum(a, b) {
+  return a + b;
+}
+
+const callBackSum = (a, b) => {
+  return a + b;
+};
+
+console.log(nameLess(1, 2));
+console.log(Sum(2, 6));
+console.log(callBackSum(9, 3));
