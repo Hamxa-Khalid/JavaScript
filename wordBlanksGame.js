c6ff44ec183eb97ed0458e7c1f3ea9257d1b562f
Hamxa-Khalid <hamxi8381@gmail.com>
2023-07-27 20:33:54 +0500

    Created JS Code for check even valude & word Blank Games

---
+++ wordBlanksGame.js
@@ -0,0 +1,6 @@
+//Create a function that takes four string arguments (noun, adjective, verb, and adverb) and returns a sentence using those words.
+function wordBlanksGame(noun, adjective, verb, adverb) {
+  return `${adjective} ${noun} ${verb} ${adverb}`;
+}
+
+console.log(wordBlanksGame("Hamza", "Love", "doing", "code"));
