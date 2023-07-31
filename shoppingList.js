2c0807045cc00b55fc32a70c630b3f8266b8c08b
Hamxa-Khalid <hamxi8381@gmail.com>
2023-07-31 14:11:26 +0500

    created shopping list and accessing object program

--- shoppingList.js
+++ shoppingList.js
@@ -1,3 +1,24 @@
-const myList = [];
-myList.push( [ "Choclate", 10], [ "Biscuit", 5], [ "Milk", 8], [ "Eggs", 12], [ "Bread", 1], )
-console.log(myList)
+const myList = [
+  ["Choclate", 10],
+  ["Biscuit", 5],
+  ["Milk", 8],
+  ["Eggs", 12],
+  ["Bread", 1],
+];
+
+function removeItem(list) {
+  list.pop();
+}
+
+function addItem(list) {
+  let item = ["coffe", 1];
+  list.push(item);
+}
+
+function displayItem(list) {
+  console.log(list);
+}
+
+removeItem(myList);
+addItem(myList);
+displayItem(myList);
